8328f22a3021fcd230b204ba9c124b590a9fb739
mazurkate8 <mazurkate8@gmail.com>
2021-05-30 14:00:15 +0300

    Created Calculate

---
+++ src/Calculate.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+public class Calculate{
+
+public static void main (String[] args){
+
+   System.out.println( "введ≥ть перше число  ");
+   Scanner scan = new Scanner(System.in);
+   int number1  = scan.nextInt();
+   System.out.println("¬вед≥ть друге число   ");
+   int number2 = scan.nextInt();
+System.out.println(number1+number2);
+}
+}
